8536bbe9b86928960b5074f77eeab8057ecde1d5
csvk <chanda.souvik@gmail.com>
2017-03-13 13:36:17 +0530

    COntinuous contracts all v0.1

--- source/currderivs.py
+++ source/currderivs.py
@@ -13,9 +13,10 @@ import pandas as pd
 import pickle as pkl
 
 URL = 'https://www.nseindia.com/archives/cd/bhav/'
-PATH = 'data/currderivs/'
-DBF_PATH = 'data/currderivs/dbf/'
-CSV_PATH = 'data/currderivs/csv/'
+PATH = 'data/currderivs/fresh/'
+DBF_PATH = 'data/currderivs/fresh/dbf/'
+CSV_PATH = 'data/currderivs/fresh/csv/'
+CSV_BKP_PATH = 'data/currderivs/fresh/csv_bkp/'
 LOGFILE = 'log.csv'
 NEW_FILENAME_FORMAT = 'CD_BhavcopyDDMMYY.zip'
 OLD_FILENAME_FORMAT = 'CD_NSEUSDINRDDMMYY.dbf.zip'
@@ -82,16 +83,36 @@ def get_bhavcopy(date_range):
 
     write_log()
 
+def csv_copy_with_bkp():
+
+    utils.mkdir(CSV_BKP_PATH)
+
+    csv_files = [f for f in os.listdir(PATH) if f.endswith('.csv')]
+    print('Initiating move for ', len(csv_files), ' files...')
+
+    bkp, move = 0, 0
+    for file in csv_files:
+        if os.path.isfile(CSV_PATH + file):
+            os.rename(PATH + file, CSV_BKP_PATH + file)
+            print(PATH + file + ' moved to ' + CSV_BKP_PATH + file)
+            bkp += 1
+        else:
+            os.rename(PATH + file, CSV_PATH + file)
+            print(PATH + file + ' moved to ' + CSV_PATH + file)
+            move += 1
+    print('{} files backed up, {} files copied'.format(bkp, move))
 
 
 def dbf_to_csv(dbf_path=DBF_PATH, csv_path=CSV_PATH):
+
+    utils.mkdir(csv_path)
 
     dbf_files = [f for f in os.listdir(dbf_path) if f.endswith('.dbf')]
 
     for file in dbf_files:
         csv_records = dbfhandler.dbf_to_csv('{}{}'.format(dbf_path, file))
 
-        csv_file = open('{}{}.csv'.format(csv_path, file[:-4]), 'a')
+        csv_file = open('{}{}.csv'.format(csv_path, file[:-4]), 'w')
         csv_file.writelines(csv_records)
         csv_file.close()
         print('File written: {}'.format('{}{}.csv'.format(csv_path, file[:-4])))
@@ -103,11 +124,12 @@ def clean_csv():
     utils.mkdir(UNCLEANED)
 
     csv_files = [f for f in os.listdir(os.curdir) if f.endswith('.csv')]
+    print('Initiating cleaning of {} files'.format(len(csv_files)))
 
+    success, error = 0, 0
     for file in csv_files:
         try:
             df = pd.read_csv(file)
-            df_bkp = df
             df['Date'] = [dates.ddmmyy_to_yyyy_mm_dd(file[-10:][:6])] * len(df['CONTRACT_D']) # Extract date from filename
 
             df['Symbol'] = df['CONTRACT_D'].str[0:12]
@@ -125,11 +147,14 @@ def clean_csv():
             df.drop('CONTRACT_D', axis=1, inplace=True)
 
             df.to_csv('{}{}'.format(CLEANED, file), sep=',', index=False)
-            df_bkp.to_csv('{}{}'.format(UNCLEANED, file), sep=',', index=False)
             os.remove(file)
             print(df['Date'][0], ',File Cleaned')
+            success += 1
         except:
             print(df['Date'][0], ',Error in cleaning')
+            error += 1
+
+    print('{} files cleaned, {} errors'.format(success, error))
 
 
 def format_csv_futures(*columns):
@@ -142,6 +167,7 @@ def format_csv_futures(*columns):
 
     cols = [c for c in columns]
 
+    success, error = 0, 0
     for file in csv_files:
         try:
             df = pd.read_csv(file)
@@ -149,29 +175,45 @@ def format_csv_futures(*columns):
             df = df.reindex_axis(cols, axis=1)
             df.to_csv('{}{}'.format(FORMATTED, file), sep=',', index=False)
             print(date, ',File formatted', file)
+            success += 1
         except:
             print(date, ',Error in formatting', file)
+            error += 1
+
+    print('{} files formatted, {} errors'.format(success, error))
 
 
 def ren_csv_files():
 
     csv_files = [f for f in os.listdir(os.curdir) if f.find('OP') < 0 and f.endswith('.csv')]
 
+    success, error = 0, 0
     for file in csv_files:
         try:
             new_name = '{}.csv'.format(dates.ddmmyy_to_yyyy_mm_dd(file[-10:][:6]))
             os.rename(file, new_name)
             print(new_name, 'file renamed')
+            success += 1
         except:
             print(new_name, 'file rename failed')
+            error += 1
 
+    print('{} files renamed, {} errors'.format(success, error))
 
+def select_expiry(expiry_dates, date, symbol, delta, series=0):
+
+    expiry_index = 0
+    for expiry in expiry_dates[symbol]:
+        if expiry > dates.relativedate(date, days=delta):
+            print('select_expiry', symbol, date, delta, series, expiry_dates[symbol][expiry_index + series])
+            return expiry_dates[symbol][expiry_index + series]
+        expiry_index += 1
 
 def select_near_expiry(expiry_dates, date, symbol, delta):
 
     for expiry in expiry_dates[symbol]:
         if expiry > dates.relativedate(date, days=delta):
-            #print('select_near_expiry', symbol, date, delta, expiry)
+            # print('select_near_expiry', symbol, date, delta, expiry)
             return expiry
 
 def select_far_expiry(expiry_dates, date, symbol, delta):
@@ -186,9 +228,118 @@ def select_far_expiry(expiry_dates, date, symbol, delta):
             return expiry
 
 
-def continuous_contracts_all(near_delta=0, far_delta=10):
+def continuous_contracts(delta=0):
     """
     Create continuous contracts file for near and far series
+    :param delta: Contract switch day difference from expiry day
+    :return: None, Create continuous contracts file
+    """
+
+    if not os.path.isfile(EXPIRIES):
+        write_expiries()
+    expiry_dates = read_expiries(EXPIRIES)
+    print(expiry_dates)
+
+    utils.mkdir(CONTINUOUS)
+
+    csv_files = [f for f in os.listdir(os.curdir) if f.endswith('.csv')]
+
+    print('Initiating continuous contract creation for {} days'.format(len(csv_files)))
+
+    near_exp, far_exp = {}, {}  # '1900-01-01', '1900-01-01' # Initialize
+
+    success, error = 0, 0
+    for file in csv_files:
+        try:
+            date = file[0:10]
+            df = pd.read_csv(file)
+            date_pd = pd.DataFrame()
+            for symbol in df['Symbol'].unique():
+                if symbol not in near_exp:
+                    near_exp[symbol], far_exp[symbol] = '1900-01-01', '1900-01-01'  # Initialize
+
+                if near_exp[symbol] <= dates.relativedate(date, days=delta):
+                    near_exp[symbol] = select_expiry(expiry_dates, date, symbol, delta, 0)
+                    far_exp[symbol] = select_expiry(expiry_dates, date, symbol, delta, 1)
+                series1 = df.loc[(df['Symbol'] == symbol) & (df['Expiry'] == near_exp[symbol])]
+                series2 = df.loc[(df['Symbol'] == symbol) & (df['Expiry'] == far_exp[symbol])]
+                series1['Symbol'], series2['Symbol'] = series1['Symbol'] + '-I', series2['Symbol'] + '-II'
+                if date_pd.empty:
+                    date_pd = pd.concat([series1, series2], axis=0)
+                else:
+                    date_pd = pd.concat([date_pd, series1, series2], axis=0)
+            date_pd.to_csv('{}{}'.format(CONTINUOUS, file), sep=',', index=False)
+            print(date, ',Continuous contract created', file)
+            success += 1
+        except:
+            print(date, ',Error creating Continuous contract', file)
+            error += 1
+
+    print('Contract created for {} days, {} errors'.format(success, error))
+
+
+def continuous_contracts_all(delta=None):
+    """
+    Create continuous contracts file for near and far series
+    :param delta: List of Contract switch day differences from expiry day
+    :return: None, Create continuous contracts file
+    """
+
+    if delta is None:
+        delta = [0]
+
+    if not os.path.isfile(EXPIRIES):
+        write_expiries()
+    expiry_dates = read_expiries(EXPIRIES)
+    print(expiry_dates)
+
+    utils.mkdir(CONTINUOUS)
+
+    csv_files = [f for f in os.listdir(os.curdir) if f.endswith('.csv')]
+
+    print('Initiating continuous contract creation for {} days'.format(len(csv_files)))
+
+    #near_exp, far_exp = {}, {}  # '1900-01-01', '1900-01-01' # Initialize
+
+    exp = [{}]
+
+    success, error = 0, 0
+    for file in csv_files:
+        try:
+            date = file[0:10]
+            df = pd.read_csv(file)
+            date_pd = pd.DataFrame()
+            for symbol in df['Symbol'].unique():
+                if symbol not in exp[0]:
+                    for d in delta:
+                        if d > 0:
+                            exp.append({})
+                        exp[d][symbol] = '1900-01-01'  # Initialize
+
+                series = []
+                for d in delta:
+                    if exp[d][symbol] <= dates.relativedate(date, days=d):
+                        exp[d][symbol] = select_expiry(expiry_dates, date, symbol, d, 0)
+                    series.append(df.loc[(df['Symbol'] == symbol) & (df['Expiry'] == exp[d][symbol])])
+                    series[d]['Symbol'] = series[d]['Symbol'] + '-' + 'I' * d
+                    date_pd = pd.concat([date_pd, series[d]], axis=0)
+
+            print('###')
+            date_pd.to_csv('{}{}'.format(CONTINUOUS, file), sep=',', index=False)
+            print(date, ',Continuous contract created', file)
+            success += 1
+        except:
+            print(date, ',Error creating Continuous contract', file)
+            error += 1
+
+
+
+    print('Contract created for {} days, {} errors'.format(success, error))
+
+
+def continuous_contracts_far_switch(near_delta=0, far_delta=10):
+    """
+    Create continuous contracts file for near and far series, with far series switching on far_delta days
     :param near_delta: Near Contract switch day difference from expiry day
     :param far_delta: Far Contract switch day as month calendar day
     :return: None, Create continuous contracts file
@@ -203,8 +354,11 @@ def continuous_contracts_all(near_delta=0, far_delta=10):
 
     csv_files = [f for f in os.listdir(os.curdir) if f.endswith('.csv')]
 
+    print('Initiating continuous contract creation for {} days'.format(len(csv_files)))
+
     near_exp, far_exp = {}, {} #'1900-01-01', '1900-01-01' # Initialize
 
+    success, error = 0, 0
     for file in csv_files:
         try:
             date = file[0:10]
@@ -235,9 +389,12 @@ def continuous_contracts_all(near_delta=0, far_delta=10):
                     date_pd = pd.concat([date_pd, series1, series2], axis=0)
             date_pd.to_csv('{}{}'.format(CONTINUOUS, file), sep=',', index=False)
             print(date, ',Continuous contract created', file)
+            success += 1
         except:
             print(date, ',Error creating Continuous contract', file)
+            error += 1
 
+    print('Contract created for {} days, {} errors'.format(success, error))
 
 def write_expiries(e_file=EXPIRIES):
 
--- source/main.py
+++ source/main.py
@@ -9,41 +9,41 @@ import dates
 import currderivs as cd, eqderivs as ed
 
 ROOT = 'C:/Users/Souvik/OneDrive/Python/nsedata/'
-PATH = 'data/currderivs/csv files/'
+PATH = 'data/currderivs/fresh/'
 
 
 os.chdir(ROOT)
 
 #### Steps Start for Currderivs download
 
-# print('Initiating NSE Currency derivatives bhavcopy download')
-# date_range = dates.dates('2011-12-03')
-# date_range = dates.adhoc_dates
-# cd.get_bhavcopy(date_range)
-# cd.dbf_to_csv()
+#print('Initiating NSE Currency derivatives bhavcopy download')
+#date_range = dates.dates('2009-01-07')
+#date_range = dates.adhoc_dates
+#cd.get_bhavcopy(date_range)
+#DBF_PATH = 'data/currderivs/fresh/dbftest/'
+#CSV_PATH = 'data/currderivs/fresh/csvtest/'
+#cd.dbf_to_csv(DBF_PATH, CSV_PATH)
+#cd.dbf_to_csv()
+#cd.csv_copy_with_bkp()
 
 #### Steps End for Currderivs download
 
 #### Steps Start for Currderivs edit
-#os.chdir(PATH)
+os.chdir(PATH + 'csv/')
 #cd.clean_csv()
-#os.chdir('cleaned/')
+os.chdir('cleaned/')
 #cd.format_csv_futures('Symbol', 'Date', 'OPEN_PRICE', 'HIGH_PRICE', 'LOW_PRICE', 'CLOSE_PRIC', 'TRD_NO_CON',
 #                              'OI_NO_CON', 'Expiry')
-#os.chdir('formatted/')
+os.chdir('formatted/test/')
 #cd.ren_csv_files()
 
 ### Steps End for Currderivs edit
 
+### Steps for creating continuous contract
 #os.chdir(PATH + 'cleaned/formatted/')
-os.chdir(PATH + 'cleaned/formatted/')
-#cd.continuous_contracts(1)
-cd.continuous_contracts_all()
+cd.continuous_contracts_all([0,1,2])
 
-
-
-
-
+### Steps end for continuous contract
 
 
 # print('Initiating NSE Equity derivatives bhavcopy download')
